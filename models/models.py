43dccc5ce5979dfb7ad3a479eb67c0c1c688add5
KevinMind <kmeinhardt7@gmail.com>
2017-10-18 16:16:47 -0500

    cleanup

--- models/models.py
+++ models/models.py
@@ -1,21 +1,10 @@
 import datetime
 import os
 
-<<<<<<< HEAD
+
 import peewee as pw
-=======
-import peewee as peewee
->>>>>>> e3045b199f38cf66e3ecf00de291dc2fdd753a35
 from playhouse.db_url import connect
 
-# Connect To DB
-<<<<<<< HEAD
-# DB = connect(os.environ.get('DATABASE') or 'dog-shelter-app.c1ga7ut5sy84.us-east-1.rds.amazonaws.com')
-
-DB = pw.MySqlDatabase("mydb", host = "dog-shelter-app.c1ga7ut5sy84.us-east-1.rds.amazonaws.com", port=5432, user="dogshelteruser", passwd="dogshelterpw")
-
-DB.connect()
-=======
 # DB = connect(
 #   os.environ.get(
 #     'DATABASE_URL',
@@ -34,7 +23,6 @@ myDB = peewee.MySQLDatabase("mydb", host="arn:aws:rds:us-east-1:956377352119:db:
 
 # when you're ready to start querying, remember to connect
 myDB.connect()
->>>>>>> e3045b199f38cf66e3ecf00de291dc2fdd753a35
 
 # Base Data Model
 class BaseModel (peewee.Model):
